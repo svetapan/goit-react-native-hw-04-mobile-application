ad48718dbc8ecb401071fda739e6b1f2435e50ff
Svetlana Panenko <svetabagira1986@gmail.com>
2023-06-26 23:57:57 +0300

    fixed buttons and MapScreen

---
+++ assets/Screens/MapScreen.jsx
@@ -0,0 +1,11 @@
+import { View, Text} from "react-native";
+
+const MapScreen = () => {
+    return (
+        <View>
+            <Text>MapScreen</Text>
+        </View>
+    )
+}
+
+export default MapScreen;
